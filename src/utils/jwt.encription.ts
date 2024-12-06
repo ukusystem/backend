239b437b6ab2db66aef1fb0e06a440d0ebc1e2ec
antony <antony.jaramillo.a@uni.pe>
2024-12-06 17:30:40 -0500

    fix(jwtencrypt): modify iv as a constant

--- src/utils/jwt.encription.ts
+++ src/utils/jwt.encription.ts
@@ -4,7 +4,7 @@ import { appConfig } from '../configs';
 export class JwtEncription {
   static readonly secretKey: Uint8Array = new Uint8Array(crypto.createHash('sha256').update(appConfig.jwt.encrypt.secret).digest()); // Hash a 32 bytes
 
-  static readonly iv: Uint8Array = new Uint8Array(crypto.randomBytes(16)); // IV debe ser de 16 bytes
+  static readonly iv: Uint8Array = new Uint8Array(16); // IV debe ser de 16 bytes
 
   static encrypt(token: string): string {
     const cipher = crypto.createCipheriv('aes-256-cbc', JwtEncription.secretKey, JwtEncription.iv);
