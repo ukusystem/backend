44db2e448f13d2aa7c9e9a88afa35ebca9f191ac
OscarJaramillo <antony.jaramillo.a@uni.pe>
2025-04-03 16:41:48 -0500

    feat(services): add sockets for temperature and energy

--- src/controllers/socket/energia/modulo.energia.manager.ts
+++ src/controllers/socket/energia/modulo.energia.manager.ts
@@ -2,19 +2,8 @@ import { MySQL2 } from '../../../database/mysql';
 import { Init } from '../../../models/init';
 import { genericLogger } from '../../../services/loggers';
 import { filterUndefined } from '../../../utils/filterUndefined';
-import {
-  MedEneListAction,
-  MedEnergiaSocketDTO,
-  MedEnergiaObserver,
-  SocketMedEnergia,
-  CtrlMedEnergiaMap,
-  ObserverMedEnergiaMap,
-  MedEnergiaObj,
-  MedEnergiaMap,
-  MedEnergiaAddUpdateDTO,
-  MedEneState,
-  MedidorEnergiaRowData,
-} from './modulo.energia.types';
+import { EnergyManager } from '../energy.region/energy.manager';
+import { MedEneListAction, MedEnergiaSocketDTO, MedEnergiaObserver, SocketMedEnergia, CtrlMedEnergiaMap, ObserverMedEnergiaMap, MedEnergiaObj, MedEnergiaMap, MedEnergiaAddUpdateDTO, MedEneState, MedidorEnergiaRowData } from './modulo.energia.types';
 
 export class ModuloEnergiaObserver implements MedEnergiaObserver {
   #socket: SocketMedEnergia;
@@ -73,6 +62,7 @@ export class MedidorEnergiaManager {
     // notifications:
     if (medidor.activo === MedEneState.Activo) {
       MedidorEnergiaManager.notifyListMedEnergia(ctrl_id, medidor, 'add');
+      EnergyManager.notifyEnergy(ctrl_id, medidor, 'add');
     }
   }
 
@@ -93,12 +83,15 @@ export class MedidorEnergiaManager {
         if (hasChangeActivo) {
           if (activo === MedEneState.Activo) {
             MedidorEnergiaManager.notifyListMedEnergia(ctrl_id, medEnergia, 'delete');
+            EnergyManager.notifyEnergy(ctrl_id, medEnergia, 'delete');
           }
           if (activo === MedEneState.Desactivado) {
             MedidorEnergiaManager.notifyListMedEnergia(ctrl_id, medEnergia, 'add');
+            EnergyManager.notifyEnergy(ctrl_id, medEnergia, 'add');
           }
         }
         MedidorEnergiaManager.notifyMedEnergia(ctrl_id, medEnergia);
+        EnergyManager.notifyEnergy(ctrl_id, medEnergia, 'update');
       }
     }
   }
@@ -112,6 +105,7 @@ export class MedidorEnergiaManager {
         if (hasDeleted) {
           // notifications:
           MedidorEnergiaManager.notifyListMedEnergia(ctrl_id, medEnergia, 'delete');
+          EnergyManager.notifyEnergy(ctrl_id, medEnergia, 'delete');
         }
         return hasDeleted;
       }
@@ -148,16 +142,7 @@ export class MedidorEnergiaManager {
 
     if (currController === undefined) {
       //  only add
-      if (
-        activo !== undefined &&
-        amperaje !== undefined &&
-        descripcion !== undefined &&
-        fdp !== undefined &&
-        frecuencia !== undefined &&
-        potenciakwh !== undefined &&
-        potenciaw !== undefined &&
-        voltaje !== undefined
-      ) {
+      if (activo !== undefined && amperaje !== undefined && descripcion !== undefined && fdp !== undefined && frecuencia !== undefined && potenciakwh !== undefined && potenciaw !== undefined && voltaje !== undefined) {
         MedidorEnergiaManager.#add(ctrl_id, { me_id, activo, amperaje, descripcion, fdp, frecuencia, potenciakwh, potenciaw, voltaje });
       }
     } else {
@@ -165,16 +150,7 @@ export class MedidorEnergiaManager {
       if (hasMedEnergia) {
         MedidorEnergiaManager.#update(ctrl_id, me_id, medidor);
       } else {
-        if (
-          activo !== undefined &&
-          amperaje !== undefined &&
-          descripcion !== undefined &&
-          fdp !== undefined &&
-          frecuencia !== undefined &&
-          potenciakwh !== undefined &&
-          potenciaw !== undefined &&
-          voltaje !== undefined
-        ) {
+        if (activo !== undefined && amperaje !== undefined && descripcion !== undefined && fdp !== undefined && frecuencia !== undefined && potenciakwh !== undefined && potenciaw !== undefined && voltaje !== undefined) {
           MedidorEnergiaManager.#add(ctrl_id, { me_id, activo, amperaje, descripcion, fdp, frecuencia, potenciakwh, potenciaw, voltaje });
         }
       }
@@ -203,60 +179,46 @@ export class MedidorEnergiaManager {
   }
 }
 
-// (async ()=>{
-//     // setInterval(()=>{
-//     //     const getRandomNumber = (min: number, max: number) => {
-//     //       return Math.floor(Math.random() * (max - min + 1)) + min;
-//     //     };
-//     //     const randomVoltaje = getRandomNumber(110,120)
-//     //     let newMedEnergia = new MedEnergiaVO({
-//     //       ctrl_id: 1,
-//     //       me_id: 1,
-//     //       voltaje: randomVoltaje,
-//     //       amperaje: 0,
-//     //       fdp: 0,
-//     //       frecuencia: 0,
-//     //       potenciaw: 0,
-//     //       potenciakwh: 0,
-//     //       activo: 1,
-//     //       descripcion: "Medidor 1",
-//     //     });
-//     //     console.log("actulizando: ", newMedEnergia.ctrl_id, newMedEnergia.me_id, newMedEnergia.voltaje)
-//     //     ModuloEnergiaManager.add_update(newMedEnergia)
-
-//     // },10000);
-
-//     // setTimeout(() => {
-//     //     let newMedEnergia = new MedEnergiaVO({
-//     //         ctrl_id: 1,
-//     //         me_id: 1,
-//     //         voltaje: 100,
-//     //         amperaje: 0,
-//     //         fdp: 0,
-//     //         frecuencia: 0,
-//     //         potenciaw: 0,
-//     //         potenciakwh: 0,
-//     //         activo: 1,
-//     //         descripcion: "Medidor 1",
-//     //       });
-//     //       ModuloEnergiaManager.delete(newMedEnergia)
-
-//     // }, 10000);
-
-//     // setTimeout(() => {
-//     //     let newMedEnergia = new MedEnergiaVO({
-//     //         ctrl_id: 1,
-//     //         me_id: 1,
-//     //         voltaje: 100,
-//     //         amperaje: 0,
-//     //         fdp: 0,
-//     //         frecuencia: 0,
-//     //         potenciaw: 0,
-//     //         potenciakwh: 0,
-//     //         activo: 1,
-//     //         descripcion: "Medidor 1",
-//     //       });
-//     //       ModuloEnergiaManager.add_update(newMedEnergia)
-
-//     // }, 30000);
-// })()
+// (async () => {
+//   setInterval(() => {
+//     const getRandomNumber = (min: number, max: number) => {
+//       return Math.floor(Math.random() * (max - min + 1)) + min;
+//     };
+//     const randomVoltaje = getRandomNumber(110, 120);
+//     const randomAmp = getRandomNumber(5, 10);
+//     const newMedEnDTO: MedEnergiaAddUpdateDTO = {
+//       me_id: 1,
+//       voltaje: randomVoltaje,
+//       amperaje: randomAmp,
+//       fdp: 0,
+//       frecuencia: 0,
+//       potenciaw: 0,
+//       potenciakwh: 0,
+//       activo: 1,
+//       descripcion: `Medidor 1 ${randomVoltaje}`,
+//     };
+//     const ctrl_id = 1;
+//     console.log('actulizando: ', ctrl_id, newMedEnDTO);
+//     MedidorEnergiaManager.add_update(ctrl_id, newMedEnDTO);
+//   }, 10000);
+//   setTimeout(() => {
+//     console.log('Eliminando', 1, 4);
+//     MedidorEnergiaManager.delete(1, 4);
+//   }, 40000);
+//   setTimeout(() => {
+//     const newMedEnDTO: MedEnergiaAddUpdateDTO = {
+//       me_id: 30,
+//       voltaje: 120,
+//       amperaje: 10,
+//       fdp: 0,
+//       frecuencia: 0,
+//       potenciaw: 0,
+//       potenciakwh: 0,
+//       activo: 1,
+//       descripcion: `Medidor Nuevo`,
+//     };
+//     const ctrl_id = 10;
+//     console.log('Agregando ====', ctrl_id, newMedEnDTO);
+//     MedidorEnergiaManager.add_update(ctrl_id, newMedEnDTO);
+//   }, 30000);
+// })();
